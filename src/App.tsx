28ceae3d6497a9a3c54d39996a90181fb36967be
= <=>
2023-07-27 16:31:29 -0300

    feat: add PomodoroCycleProvider

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,18 @@ import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
 import { Router } from './Route'
 
-import { CycleProvider } from './contexts/CycleContext'
+import { PomodoroCycleProvider } from './contexts/PomodoroCycleContext'
 import { GlobalStyles } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <GlobalStyles  />
+      <GlobalStyles />
       <BrowserRouter>
-        <CycleProvider>
+        <PomodoroCycleProvider>
           <Router />
-        </CycleProvider>
+        </PomodoroCycleProvider>
       </BrowserRouter>
     </ThemeProvider>
   )
