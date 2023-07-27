e614a62e1652b4722d8fc21e4b2daae52620c03e
= <=>
2023-07-27 16:34:31 -0300

    refactor: add functions that work when the mode changes

--- src/components/PomodoroTab/index.tsx
+++ src/components/PomodoroTab/index.tsx
@@ -1,20 +1,21 @@
-import { useState } from "react";
+import { usePomodoroCycles } from "../../contexts/PomodoroCycleContext";
 import { CycleControlButton, PomodoroCycleControlsContainer } from "./styles";
 
-type SelectButtonType = "pomodoro" | "short_break" | "long_break";
+type SelectButtonType = "pomodoro" | "shortBreak" | "longBreak";
 
 export const PomodoroCycleControls = () => {
-  const [selectedButton, setSelectedButton] = useState<SelectButtonType>("pomodoro");
+  const { activedCycle, stopCycle, switchMode } = usePomodoroCycles()
 
   const handleButtonSelected = (buttonType: SelectButtonType) => {
-    setSelectedButton(buttonType)
+    switchMode(buttonType)
+    stopCycle();
   }
 
   return (
     <PomodoroCycleControlsContainer>
-      <CycleControlButton isActive={selectedButton === "pomodoro"} onClick={() => handleButtonSelected("pomodoro")}>Pomodoro</CycleControlButton>
-      <CycleControlButton isActive={selectedButton === "short_break"} onClick={() => handleButtonSelected("short_break")}>Short Break</CycleControlButton>
-      <CycleControlButton isActive={selectedButton === "long_break"} onClick={() => handleButtonSelected("long_break")}>Long Break</CycleControlButton>
+      <CycleControlButton isActive={activedCycle === "pomodoro"} onClick={() => handleButtonSelected("pomodoro")}>Pomodoro</CycleControlButton>
+      <CycleControlButton isActive={activedCycle === "shortBreak"} onClick={() => handleButtonSelected("shortBreak")}>Short Break</CycleControlButton>
+      <CycleControlButton isActive={activedCycle === "longBreak"} onClick={() => handleButtonSelected("longBreak")}>Long Break</CycleControlButton>
     </PomodoroCycleControlsContainer>
   )
 }
