989f65bb1db97d4b870ee1474a05a8668ae2e0ab
= <=>
2023-07-30 01:24:32 -0300

    styles: add styles to focus, space, margin and add colors to form

--- src/components/TaskForm/styles.ts
+++ src/components/TaskForm/styles.ts
@@ -1,14 +1,35 @@
 import styled from "styled-components";
+interface IFormContainer {
+  inputError: boolean,
+}
 
-export const FormContainer = styled.div`
+export const FormContainer = styled.div<IFormContainer>`
   width: 100%;
   display: flex;
   height: 2.25rem;
 
-  margin: 0px 0px 1rem;
+  margin: 0.25rem 0px 0.5rem;
+  outline-color: red !important;
 
   & input {
-    flex: 1
+    flex: 1;
+    padding: 0 1rem;
+    border-radius: 0.25rem;
+    letter-spacing: .025rem;
+    font-weight: 500;
+    color: ${(props) => props.theme["gray-700"]};
+
+    &::placeholder {
+      color: ${(props) => props.theme["gray-700"]};
+    }
+    
+    outline: 0;
+    border: none;
+
+    &:focus{
+      border: 3px solid ${(props) =>
+    props.inputError ? props.theme["red-500"] : props.theme["green-700"]};
+    }
   }
 `
 
