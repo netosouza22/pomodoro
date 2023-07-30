4c436c6a39e7cf430ba9e6d374b6536767497e78
= <=>
2023-07-30 01:25:20 -0300

    feat: add features to handle focus and validation  in input form

--- src/components/TaskForm/index.tsx
+++ src/components/TaskForm/index.tsx
@@ -1,20 +1,48 @@
 import { Plus } from "@phosphor-icons/react";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useTask } from "../../contexts/TaskContext";
 import { AddTaskButton, FormContainer } from "./styles";
 
 export default function TaskForm() {
-  const [taskName, setTaskName] = useState<string>("");
   const { createTask } = useTask()
+
+  const taskInputRef = useRef<HTMLInputElement>(null)
+
+  const [taskName, setTaskName] = useState<string>("");
+  const [error, setError] = useState<boolean>(false);
 
   const handleTaskName = (e: React.ChangeEvent<HTMLInputElement>) => {
     setTaskName(e?.target.value)
+    setError(false);
   }
 
+  const handleCreateTask = () => {
+    if (taskName.length === 0) {
+      setError(true)
+      return;
+    }
+    createTask(taskName);
+    setTaskName("");
+  }
+
+  useEffect(() => {
+    if (error) {
+      taskInputRef.current?.focus()
+    }
+  }, [error])
+
+
   return (
-    <FormContainer>
-      <input name="task_name" type="text" onChange={(e) => handleTaskName(e)} />
-      <AddTaskButton onClick={() => createTask(taskName)}>
+    <FormContainer inputError={error}>
+      <input
+        name="task_name"
+        type="text"
+        ref={taskInputRef}
+        placeholder="ex: estudar para a prova"
+        value={taskName}
+        onChange={(e) => handleTaskName(e)}
+      />
+      <AddTaskButton onClick={handleCreateTask}>
         <Plus size={16} weight="bold" />
       </AddTaskButton>
     </FormContainer>
