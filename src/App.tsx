a88f47dc34c8e3e531066d1e8ec71a3f5acb75b2
Neto Sousa <netosouzacp@gmail.com>
2023-08-01 17:41:13 -0300

    feat: add basename

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyles />
-      <BrowserRouter>
+      <BrowserRouter basename='/pomodoro/'>
         <PomodoroCycleProvider>
           <TaskProvider>
             <Router />
